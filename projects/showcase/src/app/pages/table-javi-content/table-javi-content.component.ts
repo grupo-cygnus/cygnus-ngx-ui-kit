a93b841da61f5d4f40a1e974fd672f06a1bd25aa
cquezada-cygnus <claudia.quezada@cygnus.cl>
2026-05-13 11:46:34 -0400

    cygnus-javi-table doc

--- projects/showcase/src/app/pages/table-javi-content/table-javi-content.component.ts
+++ projects/showcase/src/app/pages/table-javi-content/table-javi-content.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Highlight } from 'ngx-highlightjs';
+import { HighlightLineNumbers } from 'ngx-highlightjs/line-numbers';
 import { CygnusJaviTableComponent, DataTableConfig } from 'ngx-cygnus-ui/components/table';
 
 @Component({
   selector: 'app-table-javi-content',
   imports: [
-    CygnusJaviTableComponent
+    CygnusJaviTableComponent,
+    Highlight, HighlightLineNumbers,
   ],
   templateUrl: './table-javi-content.component.html',
   styleUrl: './table-javi-content.component.scss'
@@ -45,5 +48,48 @@ export class TableJaviContentComponent {
       { label: 'Eliminar seleccionados', callback: (items) => console.log(items) }
     ]
   };
+
+  arrayExample: string = `
+    data = [
+      {
+        "cod": 1,
+        "nombre": "javier",
+        "puesto": "desarrollador",
+        "ingreso": "2015"
+      },
+      {
+        "cod": 2,
+        "nombre": "claudia",
+        "puesto": "front",
+        "ingreso": "2025"
+      },
+    ];
+  `;
+
+  dataTableConfigExample: string = `
+    tableConfig: DataTableConfig = {
+      data: this.data,
+      columns: {
+        nombre:  { label: 'Nombre',  type: 'text'     },
+        monto:   { label: 'Monto',   type: 'money'     },
+        fecha:   { label: 'Fecha',   type: 'date'      },
+      },
+      viewMode: 'table',
+      useCheckboxes: true,
+      showDynamicView: true,
+      favoritesId: 'clientes',
+      actions: [
+        { label: 'Editar', className: 'bg-sky-500',  callback: (item) => console.log('edit', item) },
+        { label: 'Ver',    className: 'bg-emerald-500', callback: (item) => console.log('view', item) },
+      ],
+      globalActions: [
+        { label: 'Eliminar seleccionados', callback: (items) => console.log(items) }
+      ]
+    };
+  `;
+
+  htmlExample: string = `
+    <cygnus-javi-table [config]="tableConfig" />
+  `;
 
 }
